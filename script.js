501ae298b5a1c0e605bcb57108e67d5f0401832f
Pushpendra kumar sahu <153269194+Pushpendra2108@users.noreply.github.com>
2024-12-23 13:07:04 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+//your JS code here. If required
+ const nameInput = document.getElementById("fname");
+
+    nameInput.addEventListener("change", function () {
+      nameInput.value = nameInput.value.toUpperCase();
+    });
